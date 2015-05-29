f9c67ec4401f9dbe37906f1b9be4cae00f8e98ba
Bartek Skorulski <bartekskorulski@gmail.com>
2015-05-29 13:56:13 +0200

    adds a comment to example

--- example.R
+++ example.R
@@ -1,6 +1,12 @@
 require('twitteR')
-source('twitterAuth.R')
-source('createDB.R')
+
+# In file 'twitterAuth.R' one can assign appropriate values to variables: 
+# api_key, api_secret, access_token, access_token_secret
+
+if (file.edit('twitterAuth.R')) {
+  source('twitterAuth.R')
+}
+source('createDb.R')
 
 options(httr_oauth_cache=TRUE)
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
@@ -9,3 +15,6 @@ dm.tweets <- searchTwitter("#python", n=50, lang="es")
 
 dm.df <- twListToDF(dm.tweets)
 
+dbReadTable(con, "tweets")
+dbWriteTable(con, "tweets", dm.df, append=TRUE)
+dbReadTable(con, "tweets")
